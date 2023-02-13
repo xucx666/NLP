c4b19250d2f6040614c1d7f4249e1f0fb162fe02
xucx666 <417690967@qq.com>
2023-02-13 20:16:35 +0800

    add framework and fix args

--- classification/args.py
+++ classification/args.py
@@ -11,4 +11,7 @@ def get_args():
     parser.add_argument('--bert_dir', type=str, default=ROOT / 'pretrained_model', help='path to bert')
     parser.add_argument('--class_num', type=int, default=15, help='number of label')
     parser.add_argument('--batch_size', type=int, default=8, help='batch_size')
+    parser.add_argument('--dropout', type=float, default=0.5, help='')
+    parser.add_argument('--feature_num', type=int, default=768, help='output size of bert')
+    parser.add_argument('--fine_tune', type=str, default=False, help='whether is finetune bert')
     return parser.parse_args()
